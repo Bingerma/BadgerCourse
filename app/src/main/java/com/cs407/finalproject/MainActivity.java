66218a5c92ccc59441dc78a609262f1001072a15
EdbertWang <ewang48@wisc.edu>
2023-11-27 14:29:11 -0600

    Attempt at setting up inital page

--- app/src/main/java/com/cs407/finalproject/MainActivity.java
+++ app/src/main/java/com/cs407/finalproject/MainActivity.java
@@ -3,12 +3,48 @@ package com.cs407.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextSearch;
+    private ListView listViewResults;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        editTextSearch = findViewById(R.id.editTextSearch);
+        listViewResults = findViewById(R.id.listViewResults);
+
+        // Sample data
+        String[] data = {"Apple", "Banana", "Orange", "Mango", "Grapes"};
+
+        // Set up the adapter
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+        listViewResults.setAdapter(adapter);
+
+        // Set up the search functionality
+        editTextSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                adapter.getFilter().filter(charSequence);
+                listViewResults.setVisibility(charSequence.length() > 0 ? View.VISIBLE : View.GONE);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {}
+        });
     }
 }
