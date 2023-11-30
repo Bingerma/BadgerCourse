1e3ed9f47fac52afff3a05698850f6f009d7fefc
Yifei Zhou <yzhou458@wisc.edu>
2023-11-29 23:11:58 -0600

    schedule function implementation

---
+++ app/src/main/java/com/cs407/project/schedule.java
@@ -0,0 +1,27 @@
+package com.cs407.project;
+
+public class schedule {
+    private String date;
+    private String term;
+    private String title;
+    private String content;
+
+    public schedule(String date,String term,String title,String content){
+        this.date=date;
+        this.term=term;
+        this.title=title;
+        this.content=content;
+    }
+    public String getDate(){
+        return date;
+    }
+    public String getTerm(){
+        return term;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getContent(){
+        return content;
+    }
+}
