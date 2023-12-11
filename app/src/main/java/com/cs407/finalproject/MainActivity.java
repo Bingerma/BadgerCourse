7891d550130741d2bad38cc600f85959f14b43f9
Bingerma <zyin57@wisc.edu>
2023-12-11 03:35:24 -0600

    UI improvements
    HomeScreen: New search bar, adaptive positioning, search from keyboard, content alignment adjustment
    Lists: Adds padding, spacing

--- app/src/main/java/com/cs407/finalproject/MainActivity.java
+++ app/src/main/java/com/cs407/finalproject/MainActivity.java
@@ -4,22 +4,29 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Rect;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -40,10 +47,30 @@ public class MainActivity extends AppCompatActivity {
     private EditText editTextSearch;
     private Button searchButton;
     private String api = "https://api.madgrades.com/v1/courses?query=";
-    private ListView listViewResults;
     private ArrayAdapter<String> adapter;
     private List<String> displayList = new ArrayList<>();
     private ArrayList<Course> courseList = new ArrayList<>();
+
+    private void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if (v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+                    v.clearFocus();
+                    hideKeyboard(v);
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event);
+    }
 
 
 
@@ -52,40 +79,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editTextSearch = findViewById(R.id.editTextSearch);
-        listViewResults = findViewById(R.id.listViewResults);
-        searchButton = findViewById(R.id.Search);
-
+        editTextSearch = findViewById(R.id.searchBarEditText);
+        searchButton = findViewById(R.id.button4);
 
         // Set up the adapter
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, displayList);
 
-        listViewResults.setAdapter(adapter);
-
-        // Set item click listener to open a new activity with the selected course URL
-        listViewResults.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if (position >= 0 && position < 5) {
-                    // Open a new activity and pass the selected course URL
-//                    Intent intent = new Intent(MainActivity.this, ProfessorDetails.class);
-                    Intent intent = new Intent(MainActivity.this, ProfessorSelect.class);
-                    intent.putExtra("courseUrl", courseList.get(position).getUrl());
-                    startActivity(intent);
-                }
-            }
-        });
-
+        // Set click listener for the search button
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                Intent intent = new Intent(MainActivity.this, courseCardList.class);
-                intent.putExtra("userInput", editTextSearch.getText().toString());
-                startActivity(intent);
-
+            public void onClick(View v) {
+                performSearch();
             }
         });
+
+        // Set the listener for the Enter key on the keyboard
+        editTextSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    performSearch();
+                    return true;
+                }
+                return false;
+            }
+        });
+
     }
+
+    private void performSearch() {
+        Intent intent = new Intent(MainActivity.this, courseCardList.class);
+        intent.putExtra("userInput", editTextSearch.getText().toString().trim());
+        startActivity(intent);
+        hideKeyboard(editTextSearch);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
