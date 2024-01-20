246aec95bfe716df109dd128aa335cd029b05a76
Patrick <patrick.yin07@outlook.com>
2024-01-20 15:37:28 -0600

    Improved Code Maintainability
    Improved onCreate in ProfessorDetail. Now in modules

--- app/src/main/java/com/cs407/finalproject/ProfessorDetails.java
+++ app/src/main/java/com/cs407/finalproject/ProfessorDetails.java
@@ -40,12 +40,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
+
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.BarData;
@@ -80,6 +82,7 @@ class CardItem {
     public String getRightText() {
         return rightText;
     }
+
     public float getColorValue() {
         return colorValue;
     }
@@ -169,13 +172,13 @@ class GradeCounts {
 }
 
 
-
 class FetchData extends AsyncTask<String, Void, ArrayList<String>> {
 
 
     private WeakReference<ProfessorDetails> activityReference;
     ArrayList<String> rmpDataArray = new ArrayList<>();
     String data = "";
+
     private int interpolateColor(float value) {
         float[] from = new float[3];
         float[] to = new float[3];
@@ -198,10 +201,10 @@ class FetchData extends AsyncTask<String, Void, ArrayList<String>> {
     }
 
 
-
     public FetchData(ProfessorDetails activity) {
         this.activityReference = new WeakReference<>(activity);
     }
+
     @Override
     protected ArrayList<String> doInBackground(String... params) {
         try {
@@ -222,7 +225,7 @@ class FetchData extends AsyncTask<String, Void, ArrayList<String>> {
             }
 
             startIndex = htmlStr.indexOf("numRatings");
-            if (startIndex != -1){
+            if (startIndex != -1) {
                 int endIndex = htmlStr.indexOf(",", startIndex);
                 String value = htmlStr.substring((startIndex) + "numRatings".length() + 2, endIndex).trim();
                 rmpDataArray.add(value);
@@ -231,7 +234,7 @@ class FetchData extends AsyncTask<String, Void, ArrayList<String>> {
             }
 
             startIndex = htmlStr.indexOf("wouldTakeAgainPercent");
-            if (startIndex != -1){
+            if (startIndex != -1) {
                 int endIndex = htmlStr.indexOf(",", startIndex);
                 String value = htmlStr.substring((startIndex) + "wouldTakeAgainPercent".length() + 2, endIndex).trim();
                 rmpDataArray.add(value);
@@ -240,7 +243,7 @@ class FetchData extends AsyncTask<String, Void, ArrayList<String>> {
             }
 
             startIndex = htmlStr.indexOf("avgDifficulty");
-            if (startIndex != -1){
+            if (startIndex != -1) {
                 int endIndex = htmlStr.indexOf(",", startIndex);
                 String value = htmlStr.substring((startIndex) + "avgDifficulty".length() + 2, endIndex).trim();
                 rmpDataArray.add(value);
@@ -268,10 +271,9 @@ class FetchData extends AsyncTask<String, Void, ArrayList<String>> {
 public class ProfessorDetails extends AppCompatActivity {
     BarChart barChart;
     private Button backButton;
-    private Map<String, String> redditMap = new HashMap<>();
-//    private ListView redditList;
-    private ArrayAdapter<String> adapter;
-    private List<String> displayList = new ArrayList<>();
+    private Map<String, GradeCounts> instructorGradeMap = new HashMap<>();
+    private String apiUrl;
+    private String selectedProfessor;
 
     public void updateUIWithResults(ArrayList<String> results) {
         List<CardItem> items = new ArrayList<>();
@@ -283,28 +285,24 @@ public class ProfessorDetails extends AppCompatActivity {
         int counter = 0;
         for (String result : results) {
             float colorValue = Float.parseFloat(result);
-            if (counter == 2){
+            if (counter == 2) {
                 colorValue = colorValue / 10;
                 colorValue = colorValue / 2;
             }
-            if (counter == 1){
-                if (colorValue < 5){
+            if (counter == 1) {
+                if (colorValue < 5) {
                     colorValue = 1;
-                }
-                else if (colorValue > 5 && colorValue < 10){
+                } else if (colorValue > 5 && colorValue < 10) {
                     colorValue = 2;
-                }
-                else if (colorValue > 10 && colorValue < 15){
+                } else if (colorValue > 10 && colorValue < 15) {
                     colorValue = 3;
-                }
-                else if (colorValue > 15 && colorValue < 20){
+                } else if (colorValue > 15 && colorValue < 20) {
                     colorValue = 4;
-                }
-                else if (colorValue > 20){
+                } else if (colorValue > 20) {
                     colorValue = 5;
                 }
             }
-            if (counter == 3){
+            if (counter == 3) {
                 colorValue = 5 - colorValue;
             }
             items.add(new CardItem(names.get(counter), result, colorValue));
@@ -317,19 +315,25 @@ public class ProfessorDetails extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
     }
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        Map<String, GradeCounts> instructorGradeMap = new HashMap<>();
-
         super.onCreate(savedInstanceState);
+        //TODO: Make the layout adaptable
         setContentView(R.layout.activity_professor_detail);
+
+        initializeField();
+        setBackButton();
+        fetchData();
+    }
+
+    private void initializeField() {
         Intent intent = getIntent();
-        String apiUrl = intent.getStringExtra("courseUrl");
-        String selectedProfessor = intent.getStringExtra("selectedProfessorName");
+        apiUrl = intent.getStringExtra("courseUrl");
+        selectedProfessor = intent.getStringExtra("selectedProfessorName");
 
+    }
 
+    private void setBackButton() {
         backButton = findViewById(R.id.backDetails);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -339,7 +343,9 @@ public class ProfessorDetails extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
 
+    private void fetchData(){
         RequestQueue queue = Volley.newRequestQueue(this);
 
         StringRequest getGradeUrl = new StringRequest(Request.Method.GET, apiUrl,
@@ -383,8 +389,8 @@ public class ProfessorDetails extends AppCompatActivity {
                                                 if (instructorGradeMap.containsKey(selectedProfessor)) {
                                                     GradeCounts selectedProfGradeCounts = instructorGradeMap.get(selectedProfessor);
                                                     Log.d("MyApp", "Grades for " + selectedProfessor + ": " +
-                                                                    "A Count: " + selectedProfGradeCounts.aCount + ", " +
-                                                                    "AB Count: " + selectedProfGradeCounts.abCount + ", "
+                                                            "A Count: " + selectedProfGradeCounts.aCount + ", " +
+                                                            "AB Count: " + selectedProfGradeCounts.abCount + ", "
                                                     );
                                                     //TODO: consider removing
                                                     TextView textView = findViewById(R.id.textView);
@@ -465,5 +471,151 @@ public class ProfessorDetails extends AppCompatActivity {
 
         new FetchData(this).execute(selectedProfessor);
     }
+
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_professor_detail);
+//        Intent intent = getIntent();
+//        String apiUrl = intent.getStringExtra("courseUrl");
+//        String selectedProfessor = intent.getStringExtra("selectedProfessorName");
+//
+//
+//        backButton = findViewById(R.id.backDetails);
+//        backButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(ProfessorDetails.this, professorCardList.class);
+//                intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        RequestQueue queue = Volley.newRequestQueue(this);
+//
+//        StringRequest getGradeUrl = new StringRequest(Request.Method.GET, apiUrl,
+//                new Response.Listener<String>() {
+//                    @Override
+//                    public void onResponse(String response) {
+//                        try {
+//                            JSONObject jsonObject = new JSONObject(response);
+//                            String gradeUrl = jsonObject.getString("gradesUrl");
+//
+//                            StringRequest getGrades = new StringRequest(Request.Method.GET, gradeUrl,
+//                                    new Response.Listener<String>() {
+//                                        @Override
+//                                        public void onResponse(String response) {
+//                                            try{
+//                                                JSONObject jsonObject1 = new JSONObject(response);
+//                                                JSONArray courseOfferings = jsonObject1.getJSONArray("courseOfferings");
+//                                                for (int i = 0; i < courseOfferings.length(); i++) {
+//                                                    JSONObject courseOffering = courseOfferings.getJSONObject(i);
+//                                                    JSONArray sections = courseOffering.getJSONArray("sections");
+//                                                    for (int j = 0; j < sections.length(); j++) {
+//                                                        JSONObject section = sections.getJSONObject(j);
+//                                                        JSONArray instructors = section.getJSONArray("instructors");
+//                                                        int aCount = section.getInt("aCount");
+//                                                        int abCount = section.getInt("abCount");
+//                                                        int bCount = section.getInt("bCount");
+//                                                        int bcCount = section.getInt("bcCount");
+//                                                        int cCount = section.getInt("cCount");
+//                                                        int dCount = section.getInt("dCount");
+//                                                        int fCount = section.getInt("fCount");
+//
+//                                                        for (int k = 0; k < instructors.length(); k++) {
+//                                                            JSONObject instructor = instructors.getJSONObject(k);
+//                                                            String instructorName = instructor.getString("name");
+//                                                            GradeCounts gradeCounts = new GradeCounts(aCount, abCount, bCount, bcCount, cCount, dCount, fCount);
+//                                                            instructorGradeMap.computeIfAbsent(instructorName, val -> new GradeCounts(0, 0, 0, 0, 0, 0, 0)).updateCounts(gradeCounts);
+//                                                        }
+//                                                    }
+//                                                }
+////                                                instructorGradeMap.forEach((name, grades) -> Log.d("MyApp", name+grades.aCount + grades.abCount + grades.bCount + grades.bcCount + grades.cCount));
+//                                                if (instructorGradeMap.containsKey(selectedProfessor)) {
+//                                                    GradeCounts selectedProfGradeCounts = instructorGradeMap.get(selectedProfessor);
+//                                                    Log.d("MyApp", "Grades for " + selectedProfessor + ": " +
+//                                                                    "A Count: " + selectedProfGradeCounts.aCount + ", " +
+//                                                                    "AB Count: " + selectedProfGradeCounts.abCount + ", "
+//                                                    );
+//                                                    //TODO: consider removing
+//                                                    TextView textView = findViewById(R.id.textView);
+//                                                    String displayText = "Grades for " + selectedProfessor + ": " +
+//                                                            "A Count: " + selectedProfGradeCounts.aCount + ", " +
+//                                                            "AB Count: " + selectedProfGradeCounts.abCount + ", " +
+//                                                            "B Count: " + selectedProfGradeCounts.bCount + ", " +
+//                                                            "BC Cont: " + selectedProfGradeCounts.bcCount + ", " +
+//                                                            "C Cont: " + selectedProfGradeCounts.cCount + ", " +
+//                                                            "D Cont: " + selectedProfGradeCounts.dCount + ", " +
+//                                                            "F Cont: " + selectedProfGradeCounts.fCount + ", "
+//                                                            ;
+//                                                    textView.setText(displayText);
+//                                                    barChart = findViewById(R.id.barChart);
+//                                                    ArrayList<BarEntry> entries = new ArrayList<>();
+//                                                    entries.add(new BarEntry(0f, selectedProfGradeCounts.aCount));
+//                                                    entries.add(new BarEntry(1f, selectedProfGradeCounts.abCount));
+//                                                    entries.add(new BarEntry(2f, selectedProfGradeCounts.bCount));
+//                                                    entries.add(new BarEntry(3f, selectedProfGradeCounts.bcCount));
+//                                                    entries.add(new BarEntry(4f, selectedProfGradeCounts.cCount));
+//                                                    entries.add(new BarEntry(5f, selectedProfGradeCounts.dCount));
+//                                                    entries.add(new BarEntry(6f, selectedProfGradeCounts.fCount));
+//                                                    BarDataSet barDataSet = new BarDataSet(entries, "Label");
+//                                                    BarData barData = new BarData(barDataSet);
+//                                                    barChart.setData(barData);
+//
+//                                                    String[] labels = new String[] {"A", "AB", "B", "BC", "C", "D", "F"};
+//                                                    XAxis xAxis = barChart.getXAxis();
+//                                                    xAxis.setValueFormatter(new IndexAxisValueFormatter(labels));
+//                                                    xAxis.setGranularity(1f);
+//                                                    xAxis.setGranularityEnabled(true);
+//                                                    xAxis.setPosition(XAxis.XAxisPosition.BOTTOM); // Labels at the bottom
+//                                                    xAxis.setDrawGridLines(false);
+//
+//                                                    barChart.invalidate();
+//                                                }
+//                                            }catch (JSONException error){
+//                                                throw new RuntimeException(error);
+//                                            }
+//                                        }
+//                                    }, new Response.ErrorListener() {
+//                                @Override
+//                                public void onErrorResponse(VolleyError error) {
+//                                    throw new RuntimeException(error);
+//                                }
+//                            }) {
+//                                @Override
+//                                public Map<String, String> getHeaders() throws AuthFailureError {
+//                                    Map<String, String> headers = new HashMap<>();
+//                                    headers.put("Content-Type", "application/json");
+//                                    headers.put("Authorization", "Token token=405f8fbc02dd4b7eb560ce722c7be74a");
+//                                    return headers;
+//                                }
+//                            };
+//
+//                            queue.add(getGrades);
+//
+//                        } catch (JSONException e) {
+//                            throw new RuntimeException(e);
+//                        }
+//                    }
+//                }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                throw new RuntimeException(error);
+//            }
+//        }) {
+//            @Override
+//            public Map<String, String> getHeaders() throws AuthFailureError {
+//                Map<String, String> headers = new HashMap<>();
+//                headers.put("Content-Type", "application/json");
+//                headers.put("Authorization", "Token token=405f8fbc02dd4b7eb560ce722c7be74a");
+//                return headers;
+//            }
+//        };
+//
+//        queue.add(getGradeUrl);
+//
+//        new FetchData(this).execute(selectedProfessor);
+//    }
 }
 
