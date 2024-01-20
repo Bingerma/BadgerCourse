ea5cdc9629dd0caa64438ea5d0c7b657cb8ecd83
Patrick <patrick.yin07@outlook.com>
2024-01-20 01:07:43 -0600

    Testing Signature

--- app/src/main/java/com/cs407/finalproject/courseCardList.java
+++ app/src/main/java/com/cs407/finalproject/courseCardList.java
@@ -91,6 +91,7 @@ public class courseCardList extends AppCompatActivity {
         }
 
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
